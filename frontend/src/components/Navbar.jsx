9d3d5a02df1e42e0be8a8047768976143980cfdc
Shobin Shaju <shobinshaju2001@gmail.com>
2023-10-06 15:40:34 +0530

    Navbar modified

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = ({ onLogout }) => {
   return (
     <nav className="bg-blue-500 p-4 flex justify-between items-center">
       <div className="text-white text-2xl font-semibold">Tic-Tac-Toe</div>
-      {isTokenAvailable && ( // Render the Logout button conditionally
+      {isTokenAvailable && (
         <button
           onClick={handleLogout}
           className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
